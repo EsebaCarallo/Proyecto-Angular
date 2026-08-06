b76559784ea383a8ad424971f526952657d55fd6
Koby Weiss <kobydarkbomber@gmail.com>
2026-08-06 12:40:06 -0600

    Inicio de proyecto + Pensemos en componentes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+{
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page.component'),
+},
+
+{
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifs/pages/dashboard-page.component'),
+},
+
+
+
+{
+        path: 'search',
+        loadComponent: () =>
+          import('./gifs/pages/dashboard-page.component'),
+},
+
+{
+    path: '**',
+    redirectTo: 'dashboard',
+},
+
+];
