b98503df957d474e4a31fffa8f74bc7bc40968c9
EsebaCarallo <kobydarkbomber@gmail.com>
2026-08-13 21:12:58 -0600

    Mostrar imagenes de relleno

---
+++ src/app/gifs/components/gif-list/gif-list-item/gif-list-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-list-item',
+  imports: [],
+  template: `<p>gif-list-item works!</p>`,
+  styleUrl: './gif-list-item.css',
+})
+export class GifListItem {}
